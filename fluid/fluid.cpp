51d56c6af12b304823e2bf1999cbb0711e9847b0
SrS3R6IO <100472343@alumnos.uc3m.es>
2023-10-05 10:20:07 +0200

    changes in class particle

--- fluid/fluid.cpp
+++ fluid/fluid.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include <cmath>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -18,71 +19,50 @@ class Particula{
     double acy;
     double acz;
     double densidad;
-    Particula(double px,double py,double pz,double hvx,double hvy,double hvz,double vx,double vy,double vz, double acx,
-              double acy, double acz, double densidad){
-      this->px = px;
-      this->py = py;
-      this->pz = pz;
-      this->hvx = hvx;
-      this->hvy = hvy;
-      this->hvz = hvz;
-      this->vx = vx;
-      this->vy = vy;
-      this->vz = vz;
-      this->acx = acx;
-      this->acy = acy;
-      this->acz = acz;
-      this->densidad = densidad;
-    }
 };
 
 
 
 void argument_validator(char * argv[]){
   // checkjng the validity of the first command (nº of executions)
-const int base = 10;
-  if (!stoi(argv[0],nullptr,base)){
-    cout << "Time steps must be numeric";
-    exit(-1);
-  }
-  else if (stoi(argv[0], nullptr, base) < 0){
-    cout << "Invalid number of time steps";
-    exit(-2);
-  }
-
-  // for reading the input file
-  ifstream ifs;
-  ifs.open(argv[1],ios_base::openmode(ios_base::binary));
-  if (!ifs){
-    cout << "Can't Open file";
-    exit(-3);
-  }
-  // read the header
-  char buff[8];
-  ifs.read(reinterpret_cast<char*>(&buff),sizeof(buff));
-
-  // trying to read the file
-  int np = 9 * buff[1];
-
-  int counter = 0;
-
-  while (counter < np){
-    Particula particle;
-    ifs.read(particle.px,sizeof(np));
-    ifs.read(particle.py,sizeof(np));
-    ifs.read(particle.pz,sizeof(np));
-    ifs.read(particle.hvx,sizeof(np));
-    ifs.read(particle.hvy,sizeof(np));
-    ifs.read(particle.hvz,sizeof(np));
-    ifs.read(particle.vx,sizeof(np));
-    ifs.read(particle.vy,sizeof(np));
-    ifs.read(particle.vz,sizeof(np));
-
-    counter += 9*4;
-  }
+  const int base = 10;
+    if (!stoi(argv[0],nullptr,base)){
+      cout << "Time steps must be numeric";
+      exit(-1);
+    }
+    else if (stoi(argv[0], nullptr, base) < 0){
+      cout << "Invalid number of time steps";
+      exit(-2);
+    }
+    // for reading the input file
+    ifstream ifs(argv[1],ios_base::openmode(ios_base::binary));
+    if (!ifs){
+      cout << "Can't Open file";
+      exit(-3);
+    }
+    // read the header
+    char buff[8];
+    ifs.read(reinterpret_cast<char*>(&buff),sizeof(buff));
 
 
 
+    // trying to read the file
+    int np = 9 * buff[1];
+
+    int counter = 0;
+    while (counter < np){
+      Particula particle{};
+      ifs.read(particle.px,sizeof(np));
+      ifs.read(particle.py,sizeof(np));
+      ifs.read(particle.pz,sizeof(np));
+      ifs.read(particle.hvx,sizeof(np));
+      ifs.read(particle.hvy,sizeof(np));
+      ifs.read(particle.hvz,sizeof(np));
+      ifs.read(particle.vx,sizeof(np));
+      ifs.read(particle.vy,sizeof(np));
+      ifs.read(particle.vz,sizeof(np));
+      counter += 9*4;
+    }
 }
 
 int main(int argc, char * argv[]) {
@@ -158,7 +138,7 @@ double incremento_densidades (double h,class particula_i, class particula_j){
     }
 }
 double incremento_aceleracion (class particula_i, class particula_j, double h, double norma){
-    double distancia= sdd::pow(max(norma,std::pow(10,-12),0.5);
+    double distancia= pow(max(norma,std::pow(10,-12),0.5);
     double operador_izquierda= 15*m*std::pow(h-distancia,2)* (particula_i.densidad + particula_j.densidad- 2*(std::pow(10,-3)))
                                / (3.14*std::pow(h,6)*distancia);
     double operador_derecha= 45*0.4*m/(3.14*std::pow(h,6));
@@ -188,13 +168,13 @@ double collision_x(class particula, double xmin, double xmax, double h ){//o mej
     double dp= std::pow(10,-3);
     double cs= 3*std::pow(10,4);
     double dv= 128.0
-        if cx=0{
+        if (cx=0){
       double incremento_x= dp-x+xmin;
       if incremento>dp{
             particula.acx+= cs*incremento_x-dv*particula.vx
           };
     }
-    if cx= nx-1{
+    if (cx= nx-1){
         double incremento_x= dp-xmax+x;
         if incremento>dp{
               particula.acx-= cs*incremento_x+dv*particula.vx
@@ -207,13 +187,13 @@ double collision_y(class particula, double ymin, double ymax, double h ){//o mej
     double dp= std::pow(10,-3);
     double cs= 3*std::pow(10,4);
     double dv= 128.0
-        if cx=0{
+        if (cx=0){
       double incremento_y= dp-y+ymin;
       if incremento>dp{
             particula.acy+= cs*incremento_y-dv*particula.vy
           };
     }
-    if cy= ny-1{
+    if (cy= ny-1){
         double incremento_y= dp-ymax+y;
         if incremento>dp{
               particula.acy-= cs*incremento_y+dv*particula.vy
@@ -232,7 +212,7 @@ double collision_z(class particula, double zmin, double zmax, double h ){//o mej
             particula.acz+= cs*incremento_z-dv*particula.vz
           };
     }
-    if cz= nz-1{
+    if (cz= nz-1){
         double incremento_z= dp-zmax+z;
         if incremento>dp{
               particula.acz-= cs*incremento_z+dv*particula.vz
@@ -240,7 +220,7 @@ double collision_z(class particula, double zmin, double zmax, double h ){//o mej
     }
 }
 double colision_x1(class particula, double xmin, double xmax){
-    if cx=0{
+    if (cx=0){
         double dx= particula.x-xmin;
         if dx>=0{
             return;
@@ -249,7 +229,7 @@ double colision_x1(class particula, double xmin, double xmax){
           particula.x=xmin-dx;
         }
       }
-    if cx=nx-1{
+    if (cx=nx-1){
         double dx= xmax-particula.x;
         if dx>=0{
             return;
@@ -262,7 +242,7 @@ double colision_x1(class particula, double xmin, double xmax){
     particula.hvx=-particula.hvx
 }
 double colision_y1(class particula, double ymin, double ymax){
-    if cy=0{
+    if (cy=0){
         double dy= particula.y-ymin;
         if dy>=0{
             return;
@@ -271,7 +251,7 @@ double colision_y1(class particula, double ymin, double ymax){
           particula.y=ymin-dy;
         }
       }
-    if cy=ny-1{
+    if (cy=ny-1){
         double dy= ymax-particula.y;
         if dy>=0{
             return;
@@ -284,7 +264,7 @@ double colision_y1(class particula, double ymin, double ymax){
     particula.hvy=-particula.hvy
 }
 double colision_z1(class particula, double zmin, double zmax){
-    if cz=0{
+    if (cz=0){
         double dz= particula.z-zmin;
         if dz>=0{
             return;
@@ -293,7 +273,7 @@ double colision_z1(class particula, double zmin, double zmax){
           particula.z=zmin-dz;
         }
       }
-    if cz=nz-1{
+    if (cz=nz-1){
         double dz= zmax-particula.z;
         if dz>=0{
             return;
