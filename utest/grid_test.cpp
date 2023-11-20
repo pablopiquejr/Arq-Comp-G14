02857d9bfb29aa073e3989515026b8ca3609e396
kikealcocerdz <enrique.alcocer1203@gmail.com>
2023-11-20 01:29:52 +0100

    segfault en primeros_cálculos()

--- utest/grid_test.cpp
+++ utest/grid_test.cpp
@@ -6,27 +6,29 @@ namespace {
 
   TEST(GridTestSuite, TestReposicionamientoParticulas) {
     int const index_particle           = 1234;
-    std::vector<double> const position = {0.50000, 0.50000, 0.50000};
+    std::vector<double> const position = {1, 0, 9};
 
     // Instancia de Particula
     Particula particula;
     particula.pxyz = {
-      {0.35707, 0.434317, 0.0790976}
+      {-0.0123631, -0.0815473, 0.0266473}
     };
     particula.hvxyz = {
-      {368.779, 480.998, 57.9082}
+      { 0.0717484, 0.0139219, 0.108199}
     };
     particula.vxyz = {
-      {179.918, 239.595, 27.1105}
+      {0.0653556, 0.0152707, 0.112494}
     };
-    particula.densidad = {321.715};
-    particula.a_c      = {
+    particula.densidad = {0};
+    particula.a_c = {
       {0, -9.8, 0}
     };
-    longitud_y_masa mi_struct;
-    Grid my_cubo(mi_struct);
+    longitud_y_masa struct_test{204.0f, 4800};
+    Grid my_cubo(struct_test);
     my_cubo.primeros_calculos();
     my_cubo.reposicionar_particula(particula, index_particle, position);
-    EXPECT_EQ(particula.pxyz[0][0], -0.50000);
+    EXPECT_EQ(particula.pxyz[1234][0], -0.040281);
+    EXPECT_EQ(particula.pxyz[1234][1], -0.0650672);
+    EXPECT_EQ(particula.pxyz[1234][2], 0.00368461);
   }
 }
