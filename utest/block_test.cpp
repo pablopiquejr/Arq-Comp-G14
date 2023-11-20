0483d47414ee4fe99099cfb31726080f947dbccc
kikealcocerdz <enrique.alcocer1203@gmail.com>
2023-11-20 11:03:37 +0100

    estructura siguiendo trazas

--- utest/block_test.cpp
+++ utest/block_test.cpp
@@ -34,4 +34,56 @@ namespace {
     vec_bloque.colision_x(0, particula);
     EXPECT_EQ(particula.a_c[1][0], -42861);
   }
+  TEST(VecBloqueSuite, colision_y_test)
+  {
+    // Instancia de Vec_Bloque
+    Vec_Bloque vec_bloque;
+    vec_bloque.lista_particulas = {0};
+
+    // Instancia de Particula
+    Particula particula;
+    particula.pxyz = {
+      {0.35707, 0.434317, 0.0790976}
+    };
+    particula.hvxyz = {
+      { 368.779, 480.998, 57.9082}
+    };
+    particula.vxyz = {
+      {179.918, 239.595, 27.1105}
+    };
+    particula.densidad={
+      321.715
+    };
+    particula.a_c = {
+      {0, -9.8, 0}
+    };
+    vec_bloque.colision_x(0, particula);
+    EXPECT_EQ(particula.a_c[1][0], -42861);
+  }
+  TEST(VecBloqueSuite, colision_z_test)
+  {
+    // Instancia de Vec_Bloque
+    Vec_Bloque vec_bloque;
+    vec_bloque.lista_particulas = {0};
+
+    // Instancia de Particula
+    Particula particula;
+    particula.pxyz = {
+      {0.35707, 0.434317, 0.0790976}
+    };
+    particula.hvxyz = {
+      { 368.779, 480.998, 57.9082}
+    };
+    particula.vxyz = {
+      {179.918, 239.595, 27.1105}
+    };
+    particula.densidad={
+      321.715
+    };
+    particula.a_c = {
+      {0, -9.8, 0}
+    };
+    vec_bloque.colision_x(0, particula);
+    EXPECT_EQ(particula.a_c[1][0], -42861);
+  }
 }
--- utest/grid_test.cpp
+++ utest/grid_test.cpp
@@ -27,8 +27,81 @@ namespace {
     Grid my_cubo(struct_test);
     my_cubo.primeros_calculos();
     my_cubo.reposicionar_particula(particula, index_particle, position);
+    my_cubo.check_if_repos();
     EXPECT_EQ(particula.pxyz[1234][0], -0.040281);
     EXPECT_EQ(particula.pxyz[1234][1], -0.0650672);
     EXPECT_EQ(particula.pxyz[1234][2], 0.00368461);
   }
+
+  TEST(GridTestSuite, TestIncrementoAceleracion){
+    int const index1 = 4321;
+    int const index2 = 5678;
+    Particula particula;
+    particula.pxyz = {
+      {-0.040281, -0.0650672, 0.00368461}
+    };
+    particula.hvxyz = {
+      { -31.6856, 21.8196, -29.2818}
+    };
+    particula.vxyz = {
+      {-13.6254, 7.78405, -10.9486}
+
+    };
+    particula.densidad = {0};
+    particula.a_c = {
+      {0, -9.8, 0}
+    };
+    longitud_y_masa struct_test{204.0f, 4800};
+    Grid my_cubo(struct_test);
+    my_cubo.primeros_calculos();
+    my_cubo.incremento_aceleracion(particula, index1, index2, 0.1);
+    EXPECT_EQ(particula.a_c[1234][0],186.254);
+    EXPECT_EQ(particula.a_c[1234][1], 58.2616);
+    EXPECT_EQ(particula.a_c[1234][2], 43.118);
+  }
+
+  TEST(GridTestSuite, TestIncrementoDensidades){
+    int const index1 = 4321;
+    int const index2 = 5678;
+    Particula particula;
+    particula.pxyz = {
+      {-0.040281, -0.0650672, 0.00368461}
+    };
+    particula.hvxyz = {
+      { -31.6856, 21.8196, -29.2818}
+    };
+    particula.vxyz = {
+      {-13.6254, 7.78405, -10.9486}
+
+    };
+    particula.densidad = {0};
+    particula.a_c = {
+      {0, -9.8, 0}
+    };
+    longitud_y_masa struct_test{204.0f, 4800};
+    Grid my_cubo(struct_test);
+    my_cubo.primeros_calculos();
+    my_cubo.incremento_aceleracion(particula, index1, index2, 0.1);
+    EXPECT_EQ(particula.a_c[1234][0],186.254);
+    EXPECT_EQ(particula.a_c[1234][1], 58.2616);
+    EXPECT_EQ(particula.a_c[1234][2], 43.118);
+  }
+  TEST(GridTestSuite, TestTranformacionDensidades){
+
+  }
+  TEST(GridTestSuite, TestTransferenciaAceleracion
+
+  }
+   TEST(GridTestSuite, TestTransferenciaAceleraciones){
+
+  }
+   TEST(GridTestSuite, TestColisionesBordes) {
+
+}
+   TEST(GridTestSuite, TestMovimientoParticulas) {
+
+   }
+   TEST(GridTestSuite, TestInteracciónBordes) {
+
+   }
 }
