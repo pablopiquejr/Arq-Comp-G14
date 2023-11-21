ed595e7a86ca3f0968fb49d70848af5044927724
kikealcocerdz <100472118@alumnos.uc3m.es>
2023-11-21 13:51:08 +0100

    Fix algunas funcionesº

--- comandos_avigon/drun.sh
+++ comandos_avigon/drun.sh
@@ -3,5 +3,5 @@
 module avail
 module load gcc/12.1.0
 cd build_debug
-valgrind --tool=cachegrind --cachegrind-out-file=large5  ./fluid/fluid 5 ../../large.fld ../../output.fld
+valgrind --tool=cachegrind --cachegrind-out-file=large5  ./fluid/fluid 10 ../../large.fld ../../output.fld
 cg_annotate large5 --auto=yes
