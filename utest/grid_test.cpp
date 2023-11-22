cc4898bdda16b487a30caf8faf9b18c95347460c
kikealcocerdz <enrique.alcocer1203@gmail.com>
2023-11-22 20:31:58 +0100

    progargs_test completado

--- utest/grid_test.cpp
+++ utest/grid_test.cpp
@@ -185,20 +185,52 @@ namespace {
 
     Grid my_cubo(struct_test);
     my_cubo.primeros_calculos();
+    my_cubo.procesamiento_colisiones();
     // Valores previamente calculados
-    my_cubo.choques_entre_particulas();
     EXPECT_EQ(my_cubo.l_m.particulas.a_c[0][0],  -2905.5805024307438);
     EXPECT_EQ(my_cubo.l_m.particulas.a_c[0][1], -1559.6870149610163);
     EXPECT_EQ(my_cubo.l_m.particulas.a_c[0][2], 122.90610561474517);
   }
 
-  TEST(GridTestSuite, TestColisionesBorders) {
-
-  }
   TEST(GridTestSuite, TestMovimientoParticulas) {
+        // Generamos artificialemente 2 partículas de prueba que sabemos que colisionan,
+        // y comprobamos nuestros cálculos a mano con calculadora con los que nos da el programa
+        Particula particula;
+        Vec_Bloque my_bloque;
+        // Partícula en 1234 en el Bloque 3471
+        longitud_y_masa struct_test{204, 2};
+        struct_test.particulas.pxyz = {
+          {-0.057331390678882599, -0.080263644456863403, -0.057781580835580826},
+          { -0.064884483814239502, -0.077343977987766266, -0.058011941611766815}
 
-  }
-  TEST(GridTestSuite, TestInteraccionesBordes) {
+        };
+        struct_test.particulas.hvxyz = {
+          { 0.21121115982532501, 0.11840522289276123,0.14053094387054443},
+          {0.065619014203548431, 0.036966234445571899, 0.14385625720024109}
+
+        };
+        struct_test.particulas.vxyz = {
+          {0.21360234916210175, 0.12304694205522537,  0.13833959400653839},
+          { 0.067155852913856506, 0.044741451740264893, 0.14454919099807739}
+
+        };
+        struct_test.particulas.densidad = {0, 0};
+        struct_test.particulas.a_c = {
+          {0, -9.8000000000000007, 0},
+          {0, -9.8000000000000007, 0}
+        };
+        struct_test.particulas.bpos = {
+          {0, 0, 0},
+          {0, 0, 0}
+        };
+
+        Grid my_cubo(struct_test);
+        my_cubo.primeros_calculos();
+        my_cubo.procesamiento_colisiones();
+        my_cubo.transferencia_aceleracion();
+        // Valores previamente calculados
+        EXPECT_EQ(my_cubo.l_m.particulas.pxyz[0][0], -0.057120179519057276);
+        EXPECT_EQ(my_cubo.l_m.particulas.pxyz[0][1], -0.079839567934465408);
 
   }
 }
--- utest/progargs_test.cpp
+++ utest/progargs_test.cpp
@@ -1,74 +1,84 @@
 #include "../sim/progargs.cpp"
 #include <gtest/gtest.h>
 
-TEST(ProgArgsTestSuite, TestValidNumberOfArguments) {
-  // Supongamos que argc es 4, que es el número correcto de argumentos
-  int argc = 4;
-  EXPECT_NO_FATAL_FAILURE(check_n_arguments(argc));
-}
-
-TEST(ProgArgsTestSuite, TestInvalidNumberOfArguments) {
-  int argc = 3;
-  EXPECT_THROW(check_n_arguments(argc), std::invalid_argument);
-}
-
-
-TEST(ProgArgsTestSuite, TestValidNumber) {
-  std::string n_iterations = "100";
-  EXPECT_NO_FATAL_FAILURE(validate_number(n_iterations));
-}
-
-TEST(ProgArgsTestSuite, TestNonNumericNumber) {
-  std::string n_iterations = "abc";
-  EXPECT_THROW(validate_number(n_iterations), std::invalid_argument);
-}
-
-TEST(ProgArgsTestSuite, TestNegativeNumber) {
-  std::string n_iterations = "-10";
-  EXPECT_THROW(validate_number(n_iterations), std::out_of_range);
-}
-
-TEST(ProgArgsTestSuite, TestFileReader) {
-  std::string file_name = "test_file.bin";
-
-  // Create a temporary file for testing
-  std::ofstream output(file_name, std::ios::binary);
-  if (!output) {
-    FAIL() << "Can't create output file";
+namespace {
+  TEST(ProgArgsTestSuite, TestValidNumberOfArguments) {
+    // Supongamos que argc es 4, que es el número correcto de argumentos
+    int argc = 4;
+    EXPECT_NO_FATAL_FAILURE(check_n_arguments(argc));
   }
 
-  // Write some sample data to the file
-  float ppm = 1.0f;
-  int n_parameters = 3;
-  output.write(reinterpret_cast<const char*>(&ppm), sizeof(float));
-  output.write(reinterpret_cast<const char*>(&n_parameters), sizeof(int));
-
-  std::array<double, 3> pxyz = {{1.0f, 2.0f, 3.0f}};
-  std::array<double, 3> hvxyz = {{4.0f, 5.0f, 6.0f}};
-  std::array<double, 3> vxyz = {{7.0f, 8.0f, 9.0f}};
-
-  for (int i = 0; i < n_parameters; i++) {
-    output.write(reinterpret_cast<const char*>(&pxyz[i]), sizeof(float));
-    output.write(reinterpret_cast<const char*>(&hvxyz[i]), sizeof(float));
-    output.write(reinterpret_cast<const char*>(&vxyz[i]), sizeof(float));
+  TEST(ProgArgsTestSuite, TestInvalidNumberOfArguments) {
+    int argc = 3;
+    EXPECT_THROW(check_n_arguments(argc), std::invalid_argument);
   }
 
-  output.close();
+  TEST(ProgArgsTestSuite, TestValidNumber) {
+    std::string n_iterations = "100";
+    EXPECT_NO_FATAL_FAILURE(validate_number(n_iterations));
+  }
 
-  // Call the file_reader function
-  longitud_y_masa result = file_reader(file_name);
-  result.particulas.pxyz = {pxyz};
-  result.particulas.hvxyz = {hvxyz};
-  result.particulas.vxyz = {vxyz};
+  TEST(ProgArgsTestSuite, TestNonNumericNumber) {
+    std::string n_iterations = "abc";
+    EXPECT_THROW(validate_number(n_iterations), std::invalid_argument);
+  }
 
+  TEST(ProgArgsTestSuite, TestNegativeNumber) {
+    std::string n_iterations = "-10";
+    EXPECT_THROW(validate_number(n_iterations), std::out_of_range);
+  }
 
-  // Verify the result
-  EXPECT_EQ(result.ppm, ppm);
-  EXPECT_EQ(result.n_particulas, n_parameters);
-  EXPECT_EQ(result.particulas.pxyz[0], pxyz);
-  EXPECT_EQ(result.particulas.hvxyz[0], hvxyz);
-  EXPECT_EQ(result.particulas.vxyz[0], vxyz);
+  TEST(ProgArgsTestSuite, TestFileReader) {
+    std::string file_name = "test_file.bin";
 
-  // Clean up the temporary file
-  std::remove(file_name.c_str());
+    // Create a temporary file for testing
+    std::ofstream output(file_name, std::ios::binary);
+    if (!output) { FAIL() << "Can't create output file"; }
+
+    // Write some sample data to the file
+    float ppm        = 1.0f;
+    int n_parameters = 3;
+    output.write(reinterpret_cast<const char*>(&ppm), sizeof(float));
+    output.write(reinterpret_cast<const char*>(&n_parameters), sizeof(int));
+
+    std::array<double, 3> pxyz = {
+      {1.0f, 2.0f, 3.0f}
+    };
+    std::array<double, 3> hvxyz = {
+      {4.0f, 5.0f, 6.0f}
+    };
+    std::array<double, 3> vxyz = {
+      {7.0f, 8.0f, 9.0f}
+    };
+
+    for (int i = 0; i < n_parameters; i++) {
+      output.write(reinterpret_cast<const char*>(&pxyz[i]), sizeof(float));
+      output.write(reinterpret_cast<const char*>(&hvxyz[i]), sizeof(float));
+      output.write(reinterpret_cast<const char*>(&vxyz[i]), sizeof(float));
+    }
+
+    output.close();
+
+    // Call the file_reader function
+    longitud_y_masa result  = file_reader(file_name);
+    result.particulas.pxyz  = {pxyz};
+    result.particulas.hvxyz = {hvxyz};
+    result.particulas.vxyz  = {vxyz};
+
+    // Verify the result
+    EXPECT_EQ(result.ppm, ppm);
+    EXPECT_EQ(result.n_particulas, n_parameters);
+    EXPECT_EQ(result.particulas.pxyz[0], pxyz);
+    EXPECT_EQ(result.particulas.hvxyz[0], hvxyz);
+    EXPECT_EQ(result.particulas.vxyz[0], vxyz);
+
+    // Clean up the temporary file
+    std::remove(file_name.c_str());
+  }
+}
+
+// correr tests namespace
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
