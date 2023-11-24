771eac565538f9971a3af9735817d892bf495cbe
kikealcocerdz <enrique.alcocer1203@gmail.com>
2023-11-24 13:53:44 +0100

    FUNCIONAL TEST working

--- ftest/functional_test.cpp
+++ ftest/functional_test.cpp
@@ -12,34 +12,49 @@
 
 namespace {
 
-  TEST(FunctionalTestSuite, estimated_time_test) {
+TEST(FunctionalTestSuite, estimated_time_test) {
+  // Empezamos el cronómetro
+  auto start_time = std::chrono::high_resolution_clock::now();
+  std::string path_to_program = "cd $HOME/CLionProjects/Arq-Comp-G14/cmake-build-release/fluid && ./fluid 1000 ../../large.fld ../../out.fld"; // Reemplaza esto con la ruta a tu programa
 
-    // Empezamos el cronómetro
-    auto start_time = std::chrono::high_resolution_clock::now();
+  // Agregar permisos de ejecución al archivo
+  std::string command = path_to_program;
 
-    // Ejecutamos el proyecto con el archivo large
-    const char* command =
-        "cd ../../cmake-build-release/ && fluid/fluid 1000 ../large.fld output.fld";
-    int return_code = std::system(command);
-
-    // Verificar si el comando se ejecutó correctamente (código de retorno igual a 0)
-    EXPECT_EQ(return_code, 0);
-
-    // Acabamos de contar
-    auto end_time = std::chrono::high_resolution_clock::now();
-
-    // Calcular tiempo de ejecucion
-    auto elapsed_time =
-        std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
-
-    // Verificar si es menos que el tiempo mejorable y el óptimo
-    EXPECT_GE(elapsed_time.count(), 45 * 1000000);
-    EXPECT_LE(elapsed_time.count(), 55 * 1000000);
+  int result = std::system(command.c_str());
+  if(result != 0) {
+    std::cerr << "Command failed with exit code: " << result << std::endl;
   }
+  int return_code = std::system(command.c_str());
+
+  // Verificar si el comando se ejecutó correctamente (código de retorno igual a 0)
+  EXPECT_EQ(return_code, 0);
+
+  // Acabamos de contar
+  auto end_time = std::chrono::high_resolution_clock::now();
+
+  // Calcular tiempo de ejecucion
+  auto elapsed_time =
+      std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+
+  // Verificar si es menos que el tiempo mejorable y el óptimo
+  EXPECT_GE(elapsed_time.count(), 45 * 1000000);
+  EXPECT_LE(elapsed_time.count(), 55 * 1000000);
+}
+
+
 
   TEST(OutputFileTestSuite, VerifyFileContent) {
     // Nombre del archivo
-    std::string filename = "/home/kikealcocerdz/CLionProjects/Arq-Comp-G14/output.fld";
+
+    std::string relative_path = "../../out.fld";
+    std::filesystem::path absolute_path = std::filesystem::absolute(relative_path);
+
+    std::string command = "chmod 777 " + absolute_path.string();
+    int result = std::system(command.c_str());
+    if(result != 0) {
+      std::cerr << "Command failed with exit code: " << result << std::endl;
+    }
+    std::string filename = absolute_path.string();
 
     // Abrir el archivo
     std::ifstream file(filename);
@@ -53,7 +68,15 @@ namespace {
 
   TEST(InputLargeFileTestSuite, VerifyFileContent) {
     // Nombre del archivo
-    std::string filename = "/home/kikealcocerdz/CLionProjects/Arq-Comp-G14/large.fld";
+    std::string relative_path = "../../out.fld";
+    std::filesystem::path absolute_path = std::filesystem::absolute(relative_path);
+
+    std::string command = "chmod 777 " + absolute_path.string();
+    int result = std::system(command.c_str());
+    if(result != 0) {
+      std::cerr << "Command failed with exit code: " << result << std::endl;
+    }
+    std::string filename = absolute_path.string();
 
     // Verificar la extensión del archivo
     std::filesystem::path filePath(filename);
@@ -72,7 +95,15 @@ namespace {
 
   TEST(InputSmallFileTestSuite, VerifyFileContent) {
     // Nombre del archivo
-    std::string filename = "/home/kikealcocerdz/CLionProjects/Arq-Comp-G14/small.fld";
+    std::string relative_path = "../../out.fld";
+    std::filesystem::path absolute_path = std::filesystem::absolute(relative_path);
+
+    std::string command = "chmod 777 " + absolute_path.string();
+    int result = std::system(command.c_str());
+    if(result != 0) {
+      std::cerr << "Command failed with exit code: " << result << std::endl;
+    }
+    std::string filename = absolute_path.string();
 
     // Verificar la extensión del archivo
     std::filesystem::path filePath(filename);
